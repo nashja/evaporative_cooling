e54d8b5a196a28da270270ef6d87178db80f5000
Jordan Nash <sanfrancejan@gmail.com>
2026-01-16 08:10:41 +1100

    First Commit - Change names to evaporative cooling from integration blueprint - change to this github repository

--- custom_components/evaporative_cooling/binary_sensor.py
+++ custom_components/evaporative_cooling/binary_sensor.py
@@ -1,4 +1,4 @@
-"""Binary sensor platform for integration_blueprint."""
+"""Binary sensor platform for evaporative_cooling."""
 
 from __future__ import annotations
 
@@ -21,8 +21,8 @@ if TYPE_CHECKING:
 
 ENTITY_DESCRIPTIONS = (
     BinarySensorEntityDescription(
-        key="integration_blueprint",
-        name="Integration Blueprint Binary Sensor",
+        key="evaporative_cooling",
+        name="Evaporative cooling Binary Sensor",
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
     ),
 )
@@ -44,7 +44,7 @@ async def async_setup_entry(
 
 
 class IntegrationBlueprintBinarySensor(IntegrationBlueprintEntity, BinarySensorEntity):
-    """integration_blueprint binary_sensor class."""
+    """evaporative_cooling binary_sensor class."""
 
     def __init__(
         self,
--- custom_components/evaporative_cooling/coordinator.py
+++ custom_components/evaporative_cooling/coordinator.py
@@ -1,4 +1,4 @@
-"""DataUpdateCoordinator for integration_blueprint."""
+"""DataUpdateCoordinator for evaporative_cooling."""
 
 from __future__ import annotations
 
